e8ffddde075171fd67caccd0b6a095cbf79a767d
Malte Meister <malte@informationlogistics.co.za>
2016-05-24 11:09:10 +0200

    set up new project for molo.surveys using cookiecutter

---
+++ README.rst
@@ -0,0 +1,16 @@
+molo.surveys
+=============================
+
+.. image:: https://img.shields.io/travis/praekelt/molo.surveys.svg
+        :target: https://travis-ci.org/praekelt/molo.surveys
+
+.. image:: https://img.shields.io/pypi/v/molo.surveys.svg
+        :target: https://pypi.python.org/pypi/molo.surveys
+
+.. image:: https://coveralls.io/repos/praekelt/molo.surveys/badge.png?branch=develop
+    :target: https://coveralls.io/r/praekelt/molo.surveys?branch=develop
+    :alt: Code Coverage
+
+.. image:: https://readthedocs.org/projects/molo.surveys/badge/?version=latest
+    :target: https://molo.surveys.readthedocs.org
+    :alt: molo.surveys Docs
---
+++ setup.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('requirements.txt') as req_file:
+    requirements = req_file.read().split('\n')
+
+with open('requirements-dev.txt') as req_file:
+    requirements_dev = req_file.read().split('\n')
+
+with open('VERSION') as fp:
+    version = fp.read().strip()
+
+setup(
+    name='molo.surveys',
+    version=version,
+    description="This is the molo.surveys project.",
+    long_description=readme,
+    author="Praekelt Foundation",
+    author_email='dev@praekeltfoundation.org',
+    url='https://github.com/praekelt/molo.surveys',
+    packages=[
+        'molo.surveys',
+    ],
+    package_dir={'molo.surveys':
+                 'molo.surveys'},
+    include_package_data=True,
+    install_requires=requirements,
+    license="BSD",
+    zip_safe=False,
+    keywords='molo.surveys',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2.7',
+    ]
+)
