6cc1374b4aae0a5527ea9dd69c03ddfaf4e643e6
Malte Meister <malte@informationlogistics.co.za>
2016-05-26 10:47:43 +0200

    TUNE-89: use TextField instead of RichTextField in SurveyPage model, set submit button text

--- molo/surveys/models.py
+++ molo/surveys/models.py
@@ -1,8 +1,8 @@
+from django.db.models.fields import TextField
 from modelcluster.fields import ParentalKey
-from molo.core.models import LanguagePage, SectionPage, ArticlePage
+from molo.core.models import SectionPage, ArticlePage
 
 from wagtail.wagtailadmin.edit_handlers import FieldPanel, InlinePanel
-from wagtail.wagtailcore.fields import RichTextField
 
 from wagtailsurveys import models as surveys_models
 
@@ -12,8 +12,8 @@ ArticlePage.subpage_types += ['surveys.SurveyPage']
 
 
 class SurveyPage(surveys_models.AbstractSurvey):
-    intro = RichTextField(blank=True)
-    thank_you_text = RichTextField(blank=True)
+    intro = TextField(blank=True)
+    thank_you_text = TextField(blank=True)
 
     content_panels = surveys_models.AbstractSurvey.content_panels + [
         FieldPanel('intro', classname="full"),
