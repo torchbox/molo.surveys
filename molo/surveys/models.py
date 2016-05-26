21d4bb448be597c1fbc51a1806ec0615694d7ffd
Malte Meister <malte@informationlogistics.co.za>
2016-05-26 11:43:24 +0200

    TUNE-89: restrict surveys to logged in users, only allow a user to take a survey once

--- molo/surveys/models.py
+++ molo/surveys/models.py
@@ -1,4 +1,11 @@
+import json
+
+from django.conf import settings
+from django.core.serializers.json import DjangoJSONEncoder
+from django.db import models
+
 from django.db.models.fields import TextField
+from django.shortcuts import render
 from modelcluster.fields import ParentalKey
 from molo.core.models import SectionPage, ArticlePage
 
@@ -21,6 +28,35 @@ class SurveyPage(surveys_models.AbstractSurvey):
         FieldPanel('thank_you_text', classname="full"),
     ]
 
+    def get_submission_class(self):
+        return CustomFormSubmission
+
+    def process_form_submission(self, form):
+        self.get_submission_class().objects.create(
+            form_data=json.dumps(form.cleaned_data, cls=DjangoJSONEncoder),
+            page=self, user=form.user
+        )
+
+    def serve(self, request, *args, **kwargs):
+        if self.get_submission_class().objects.filter(
+            page=self, user__pk=request.user.pk
+        ).exists():
+            return render(
+                request,
+                self.template,
+                self.get_context(request)
+            )
+
+        return super(SurveyPage, self).serve(request, *args, **kwargs)
+
 
 class SurveyFormField(surveys_models.AbstractFormField):
     page = ParentalKey(SurveyPage, related_name='survey_form_fields')
+
+
+class CustomFormSubmission(surveys_models.AbstractFormSubmission):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('page', 'user')
