68b57fbc29ed8481baf96ae1baaa3e0d50920b9e
Malte Meister <malte@informationlogistics.co.za>
2016-06-03 17:04:22 +0200

    TUNE-89: more view tests

--- molo/surveys/tests/test_views.py
+++ molo/surveys/tests/test_views.py
@@ -21,11 +21,12 @@ class TestSurveyViews(TestCase, MoloTestCaseMixin):
             email='tester@example.com',
             password='tester')
 
-    def create_molo_survey_page(self, parent):
+    def create_molo_survey_page(self, parent, **kwargs):
         molo_survey_page = MoloSurveyPage(
             title='Test Survey', slug='test-survey',
             intro='Introduction to Test Survey ...',
-            thank_you_text='Thank you for taking the Test Survey'
+            thank_you_text='Thank you for taking the Test Survey',
+            **kwargs
         )
 
         parent.add_child(instance=molo_survey_page)
@@ -70,20 +71,10 @@ class TestSurveyViews(TestCase, MoloTestCaseMixin):
         # for test_multiple_submissions_not_allowed_by_default
         return molo_survey_page.url
 
-    def test_multiple_submissions_not_allowed_by_default(self):
-        molo_survey_page_url = self.test_submit_survey_as_logged_in_user()
-
-        response = self.client.get(molo_survey_page_url)
-
-        self.assertContains(response,
-                            'You have already completed this survey.')
-
-    def test_anonymous_submissions(self):
+    def test_anonymous_submissions_option(self):
         molo_survey_page, molo_survey_form_field = \
-            self.create_molo_survey_page(parent=self.english)
-
-        molo_survey_page.allow_anonymous_submissions = True
-        molo_survey_page.save_revision().publish()
+            self.create_molo_survey_page(parent=self.english,
+                                         allow_anonymous_submissions=True)
 
         response = self.client.get(molo_survey_page.url)
 
@@ -100,11 +91,45 @@ class TestSurveyViews(TestCase, MoloTestCaseMixin):
         # for test_multiple_submissions_not_allowed_by_default_anonymous
         return molo_survey_page.url
 
-    def test_multiple_submissions_not_allowed_by_default_anonymous(self):
-        molo_survey_page_url = self.test_anonymous_submissions()
+    def test_multiple_submissions_not_allowed_by_default(self):
+        molo_survey_page_url = self.test_submit_survey_as_logged_in_user()
 
         response = self.client.get(molo_survey_page_url)
 
         self.assertContains(response,
                             'You have already completed this survey.')
 
+    def test_multiple_submissions_not_allowed_by_default_anonymous(self):
+        molo_survey_page_url = self.test_anonymous_submissions_option()
+
+        response = self.client.get(molo_survey_page_url)
+
+        self.assertContains(response,
+                            'You have already completed this survey.')
+
+    def test_multiple_submissions_option(self, anonymous=False):
+        molo_survey_page, molo_survey_form_field = \
+            self.create_molo_survey_page(
+                parent=self.english,
+                allow_multiple_submissions_per_user=True
+            )
+
+        if not anonymous:
+            self.client.login(username='tester', password='tester')
+
+        for _ in range(2):
+            response = self.client.get(molo_survey_page.url)
+
+            self.assertContains(response, molo_survey_page.title)
+            self.assertContains(response, molo_survey_page.intro)
+            self.assertContains(response, molo_survey_form_field.label)
+
+            response = self.client.post(molo_survey_page.url, {
+                molo_survey_form_field.label.lower().replace(' ', '-'):
+                    'python'
+            })
+
+            self.assertContains(response, molo_survey_page.thank_you_text)
+
+    def test_multiple_submissions_option_anonymous(self):
+        self.test_multiple_submissions_option(True)
