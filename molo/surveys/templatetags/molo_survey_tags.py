491787e4f267c66ac6eb281d2e710197ea14174c
Mitso <mqalaba@gmail.com>
2017-02-20 20:04:13 +0200

    add survey headline tag

--- molo/surveys/templatetags/molo_survey_tags.py
+++ molo/surveys/templatetags/molo_survey_tags.py
@@ -8,6 +8,22 @@ from molo.core.templatetags.core_tags import get_pages
 
 register = template.Library()
 
+@register.inclusion_tag('surveys/surveys_headline.html', takes_context=True)
+def surveys_list_headline(context, pk=None):
+    context = copy(context)
+    locale_code = context.get('locale_code')
+    page = SurveysIndexPage.objects.live().first()
+    if page:
+        surveys = (
+            MoloSurveyPage.objects.child_of(page).filter(
+                languages__language__is_main_language=True).specific())
+    else:
+        surveys = MoloSurveyPage.objects.none()
+
+    context.update({
+        'surveys': get_pages(context, surveys, locale_code)
+    })
+    return context
 
 @register.inclusion_tag('surveys/surveys_list.html', takes_context=True)
 def surveys_list(context, pk=None):
