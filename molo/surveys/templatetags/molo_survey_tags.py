9ef9c57da95a51356102ad1d66b71cdfc8825c15
Saeed Marzban <Saeed.mb8@gmail.com>
2017-02-22 14:20:44 +0200

    surveys headline template tag

--- molo/surveys/templatetags/molo_survey_tags.py
+++ molo/surveys/templatetags/molo_survey_tags.py
@@ -9,8 +9,7 @@ from molo.core.templatetags.core_tags import get_pages
 register = template.Library()
 
 
-@register.inclusion_tag('surveys/surveys_headline.html', takes_context=True)
-def surveys_list_headline(context, pk=None):
+def get_survey_list(context):
     context = copy(context)
     locale_code = context.get('locale_code')
     page = SurveysIndexPage.objects.live().first()
@@ -25,24 +24,16 @@ def surveys_list_headline(context, pk=None):
         'surveys': get_pages(context, surveys, locale_code)
     })
     return context
+
+
+@register.inclusion_tag('surveys/surveys_headline.html', takes_context=True)
+def surveys_list_headline(context):
+    return get_survey_list(context)
 
 
 @register.inclusion_tag('surveys/surveys_list.html', takes_context=True)
 def surveys_list(context, pk=None):
-    context = copy(context)
-    locale_code = context.get('locale_code')
-    page = SurveysIndexPage.objects.live().first()
-    if page:
-        surveys = (
-            MoloSurveyPage.objects.child_of(page).filter(
-                languages__language__is_main_language=True).specific())
-    else:
-        surveys = MoloSurveyPage.objects.none()
-
-    context.update({
-        'surveys': get_pages(context, surveys, locale_code)
-    })
-    return context
+    return get_survey_list(context)
 
 
 @register.inclusion_tag('surveys/surveys_list.html', takes_context=True)
