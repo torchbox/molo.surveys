2bb04ff29f76499658c1aff0e5ef2de4d8d18714
nathanbegbie <nathanbegbie@gmail.com>
2017-06-05 11:37:18 +0200

    Added JS to prevent multi-step and display question directly being selected at the same time

--- molo/surveys/wagtail_hooks.py
+++ molo/surveys/wagtail_hooks.py
@@ -1,6 +1,8 @@
+from django.conf import settings
+from django.utils.html import format_html_join
+from django.contrib.auth.models import User
 
 from wagtail.wagtailcore import hooks
-from django.contrib.auth.models import User
 
 
 @hooks.register('construct_main_menu')
@@ -9,3 +11,14 @@ def show_surveys_entries_for_users_have_access(request, menu_items):
             pk=request.user.pk, groups__name='Moderators').exists():
         menu_items[:] = [
             item for item in menu_items if item.name != 'surveys']
+
+@hooks.register('insert_global_admin_js')
+def global_admin_js():
+    js_files = [
+        'js/survey-admin.js',
+    ]
+
+    js_includes = format_html_join('\n', '<script src="{0}{1}"></script>',
+        ((settings.STATIC_URL, filename) for filename in js_files)
+    )
+    return js_includes
