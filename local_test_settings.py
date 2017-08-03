4c7810083c286e594ba2e37b983d7fdfe154e2ec
Codie Roelf <codie@praekelt.com>
2017-08-03 18:13:05 +0200

    add tests and template tags

--- local_test_settings.py
+++ local_test_settings.py
@@ -1,5 +1,6 @@
 from .base import *
 
+
 INSTALLED_APPS += (
     'wagtailsurveys',
 )
--- molo/surveys/wagtail_hooks.py
+++ molo/surveys/wagtail_hooks.py
@@ -4,6 +4,14 @@ from django.contrib.auth.models import User
 
 from wagtail.wagtailcore import hooks
 
+from django.conf.urls import url
+from django.core.urlresolvers import reverse
+from django.utils.translation import ugettext as _
+
+from wagtail.wagtailadmin.menu import MenuItem
+
+from .views import create
+
 
 @hooks.register('construct_main_menu')
 def show_surveys_entries_for_users_have_access(request, menu_items):
@@ -24,3 +32,21 @@ def global_admin_js():
         ((settings.STATIC_URL, filename) for filename in js_files)
     )
     return js_includes
+
+
+# CSV creation hooks
+
+
+@hooks.register('register_admin_urls')
+def register_csv_group_creation_urlconf():
+    return [
+        url('csv-group-creation/$', create, name='csv-group-creation'),
+    ]
+
+
+@hooks.register('register_settings_menu_item')
+def register_csv_group_creation_menu_item():
+    return MenuItem(
+        _('CSV group creation'),
+        reverse('csv-group-creation'),
+        classnames='icon icon-group', order=601)
