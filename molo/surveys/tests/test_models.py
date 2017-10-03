f43904af5b4d19474ab86ccd31ca470465f49a25
Todd Dembrey <todd.dembrey@torchbox.com>
2017-10-03 12:31:45 +0100

    Add test to confirm that the choices are updated correctly

--- molo/surveys/tests/test_models.py
+++ molo/surveys/tests/test_models.py
@@ -1,7 +1,8 @@
 from django.test import TestCase
 from molo.core.tests.base import MoloTestCaseMixin
-from molo.surveys.models import MoloSurveyPage, MoloSurveySubmission
+from molo.surveys.models import MoloSurveyPage, MoloSurveySubmission, MoloSurveyFormField
 
+from .utils import skip_logic_data
 
 class TestSurveyModels(TestCase, MoloTestCaseMixin):
     def test_submission_class(self):
@@ -14,3 +15,35 @@ class TestSurveyModels(TestCase, MoloTestCaseMixin):
             form_data='{}'
         ).get_data()
         self.assertIn('username', data)
+
+
+class TestSkipLogicMixin(TestCase, MoloTestCaseMixin):
+    def setUp(self):
+        self.mk_main()
+        self.field_choices = ['old', 'this', 'is']
+        self.survey = MoloSurveyPage(
+            title='Test Survey',
+            slug='test-survey',
+        )
+        self.section_index.add_child(instance=self.survey)
+        self.survey.save_revision().publish()
+        self.choice_field = MoloSurveyFormField.objects.create(
+            page=self.survey,
+            sort_order=1,
+            label='Your favourite animal',
+            field_type='dropdown',
+            skip_logic=skip_logic_data(self.field_choices),
+            required=True
+        )
+
+    def test_choices_updated_from_streamfield_on_save(self):
+        self.assertEqual(
+            ','.join(self.field_choices),
+            self.choice_field.choices
+        )
+
+        new_choices = ['this', 'is', 'new']
+        self.choice_field.skip_logic = skip_logic_data(new_choices)
+        self.choice_field.save()
+
+        self.assertEqual(','.join(new_choices), self.choice_field.choices)
