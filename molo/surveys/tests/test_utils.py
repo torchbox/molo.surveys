bd02fec974e05281f6a9fcd669f3bb22226a6baa
Todd Dembrey <todd.dembrey@torchbox.com>
2017-10-09 09:56:56 +0100

    Add tests to ensure questions skip correctly

--- molo/surveys/tests/test_utils.py
+++ molo/surveys/tests/test_utils.py
@@ -42,23 +42,31 @@ class TestSkipLogicPaginator(TestCase, MoloTestCaseMixin):
             page=self.survey,
             sort_order=3,
             label='Your least favourite animal',
-            field_type='singleline',
+            field_type='dropdown',
+            skip_logic=skip_logic_data(field_choices, field_choices, question=self.fourth_field),
             required=True
         )
         self.paginator = SkipLogicPaginator(self.survey.get_form_fields())
 
     def test_correct_num_pages(self):
-        self.assertEqual(self.paginator.num_pages, 2)
+        self.assertEqual(self.paginator.num_pages, 3)
 
     def test_skip_indexes_correct(self):
-        self.assertEqual(self.paginator.skip_indexes, [0, 2, 4])
+        self.assertEqual(self.paginator.skip_indexes, [0, 2, 3, 4])
 
     def test_first_page_correct(self):
-        self.assertEqual(self.paginator.page(1).object_list, [self.first_field, self.second_field])
+        page = self.paginator.page(1)
+        self.assertEqual(page.object_list, [self.first_field, self.second_field])
+        self.assertTrue(page.has_next())
+
+    def test_second_page_correct(self):
+        page = self.paginator.page(2)
+        self.assertEqual(page.object_list, [self.third_field])
+        self.assertTrue(page.has_next())
 
     def test_last_page_correct(self):
-        last_page = self.paginator.page(2)
-        self.assertEqual(last_page.object_list, [self.third_field, self.fourth_field])
+        last_page = self.paginator.page(3)
+        self.assertEqual(last_page.object_list, [self.fourth_field])
         self.assertFalse(last_page.has_next())
 
     def test_is_end_if_skip_logic(self):
@@ -74,8 +82,50 @@ class TestSkipLogicPaginator(TestCase, MoloTestCaseMixin):
             self.survey.get_form_fields(),
             {self.second_field.clean_name: 'question'}
         )
-        second_page = paginator.page(2)
+        page = paginator.page(1)
+        next_page_number = page.next_page_number()
+        self.assertEqual(next_page_number, 3)
+        second_page = paginator.page(next_page_number)
         self.assertEqual(second_page.object_list, [self.fourth_field])
+
+    def test_previous_page_if_skip_a_page(self):
+        paginator = SkipLogicPaginator(
+            self.survey.get_form_fields(),
+            {
+                self.first_field.clean_name: 'python',
+                self.second_field.clean_name: 'question',
+            }
+        )
+        page = paginator.page(1)
+        next_page_number = page.next_page_number()
+        self.assertEqual(next_page_number, 3)
+        second_page = paginator.page(next_page_number)
+        previous_page_number = second_page.previous_page_number()
+        self.assertEqual(previous_page_number, 1)
+        self.assertEqual(
+            paginator.page(previous_page_number).object_list,
+            [self.first_field, self.second_field],
+        )
+
+    def test_question_progression_index(self):
+        paginator = SkipLogicPaginator(
+            self.survey.get_form_fields(),
+            {
+                self.first_field.clean_name: 'python',
+                self.second_field.clean_name: 'question',
+            }
+        )
+        self.assertEqual(paginator.previous_question_page, 1)
+        self.assertEqual(paginator.last_question_index, 1)
+        self.assertEqual(paginator.next_question_page, 3)
+        self.assertEqual(paginator.next_question_index, 3)
+
+    def test_no_data_index(self):
+        paginator = SkipLogicPaginator(self.survey.get_form_fields())
+        self.assertEqual(paginator.previous_question_page, 1)
+        self.assertEqual(paginator.last_question_index, 0)
+        self.assertEqual(paginator.next_question_page, 1)
+        self.assertEqual(paginator.next_question_index, 0)
 
 
 class SkipLogicPaginatorMulti(TestCase, MoloTestCaseMixin):
--- molo/surveys/utils.py
+++ molo/surveys/utils.py
@@ -28,43 +28,64 @@ class SkipLogicPaginator(Paginator):
     def num_pages(self):
         return len(self.skip_indexes) - 1
 
-    def page_skip_values(self):
-        minimum_skip = self.num_pages
-        next_question_index = 0
-        question_ids = [question.id for question in self.object_list]
-        if self.last_question_page:
-            last_question = self.object_list[self.last_question_page]
+    @cached_property
+    def next_question_index(self):
+        if self.previous_question_page:
+            question_ids = [question.id for question in self.object_list]
+            last_question = self.object_list[self.last_question_index]
             last_answer = self.data[last_question.clean_name]
             if last_question.is_next_action(last_answer, SkipState.QUESTION):
                 next_question_id = last_question.next_page(last_answer)
-                next_question_index = question_ids.index(next_question_id)
-                minimum_skip = next(i-1 for i, v in enumerate(self.skip_indexes) if v > next_question_index)
-
-        return minimum_skip, next_question_index
+                return question_ids.index(next_question_id)
+        return 0
 
     @cached_property
-    def last_question_page(self):
+    def next_question_page(self):
+        return next(
+            i for i, v in enumerate(self.skip_indexes) if v > self.next_question_index
+        )
+
+    @cached_property
+    def answered_indexes(self):
         question_labels = [question.clean_name for question in self.object_list]
-        answered_questions = [
+        return [
             question_labels.index(question) for question in self.data if question in question_labels
         ]
-        if answered_questions:
-            return max(answered_questions)
 
+    @cached_property
+    def first_last_question_index(self):
+        if self.answered_indexes:
+            return min(self.answered_indexes)
         return 0
+
+    @cached_property
+    def last_question_index(self):
+        if self.answered_indexes:
+            return max(self.answered_indexes)
+        return 0
+
+    @cached_property
+    def previous_question_page(self):
+        return next(
+            i for i, v in enumerate(self.skip_indexes) if v > self.last_question_index
+        )
 
     def page(self, number):
         number = self.validate_number(number)
-        if self.last_question_page == number:
-            minimum_skip, next_question_index = self.page_skip_values()
+        page = number - 1
+        if not self.data:
+            top_index = page + self.per_page
+            bottom = self.skip_indexes[page]
+            top = self.skip_indexes[top_index]
+        elif self.previous_question_page == number:
+            bottom = self.first_last_question_index
+            top = self.last_question_index + 1
         else:
-            minimum_skip, next_question_index = self.num_pages, 0
-
-        bottom_index = min(number - 1, minimum_skip)
-        top_index = bottom_index + self.per_page
-        bottom = max(self.skip_indexes[bottom_index], next_question_index)
-        top = self.skip_indexes[top_index]
-        return self._get_page(self.object_list[bottom:top], bottom_index + 1, self)
+            page = min(page, self.next_question_page)
+            top_index = page + self.per_page
+            bottom = max(self.skip_indexes[page], self.next_question_index)
+            top = self.skip_indexes[top_index]
+        return self._get_page(self.object_list[bottom:top], page + 1, self)
 
 
 class SkipLogicPage(Page):
@@ -97,3 +118,9 @@ class SkipLogicPage(Page):
         return redirect(
             reverse('molo.surveys:success', args=(slug, ))
         )
+
+    def next_page_number(self):
+        return self.paginator.next_question_page
+
+    def previous_page_number(self):
+        return self.paginator.previous_question_page
