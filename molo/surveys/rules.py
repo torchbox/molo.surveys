c6eea3d6336d29787b0125c7b9a30c040cceaace
Todd Dembrey <todd.dembrey@torchbox.com>
2017-10-20 12:53:50 +0100

    Update the default values on the operator choice

--- molo/surveys/rules.py
+++ molo/surveys/rules.py
@@ -250,8 +250,8 @@ class ArticleTagRule(AbstractBaseRule):
     }
 
     OPERATOR_CHOICES = (
-        (EQUALS, _('equals')),
-        (GREATER_THAN, _('greater than')),
+        (EQUALS, _('equal to')),
+        (GREATER_THAN, _('more than')),
         (LESS_THAN, _('less than')),
     )
 
@@ -262,7 +262,7 @@ class ArticleTagRule(AbstractBaseRule):
         _('operator'),
         max_length=3,
         choices=OPERATOR_CHOICES,
-        default=EQUALS,
+        default=GREATER_THAN,
     )
     count = models.IntegerField()
 
