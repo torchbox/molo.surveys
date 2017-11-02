c0b64767de2255649ccc274efae104390f4b9af4
Todd Dembrey <todd.dembrey@torchbox.com>
2017-11-02 14:52:39 +0000

    Allow multiple text boxes in the same page

--- molo/surveys/static/js/widgets/character_count.js
+++ molo/surveys/static/js/widgets/character_count.js
@@ -1,18 +1,19 @@
 (function() {
     document.addEventListener('DOMContentLoaded', function() {
         var inputs = document.querySelectorAll('.input-group input[type="text"]');
-        var span = document.createElement('span');
-        var setSpanValue = function(target) {
+        var setSpanValue = function(target, span) {
             var limit = parseInt(target.getAttribute('maxlength'));
             var current = target.value.length;
             var remaining = limit - current;
             span.textContent = remaining;
         };
         for (var i = 0; i < inputs.length; i++) {
+            var span = document.createElement('span');
             inputs[i].parentNode.insertBefore(span, inputs[i].nextSibling);
-            setSpanValue(inputs[i]);
+            setSpanValue(inputs[i], span);
             inputs[i].addEventListener("input", function(e) {
-                setSpanValue(e.target);
+                var span = e.target.nextSibling;
+                setSpanValue(e.target, span);
             },false);
         }
     }, false);
